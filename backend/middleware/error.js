cd842cb0a17ba9add0ea7205f64f32d1977ced35
shahil19 <basharshahil@gmail.com>
2022-09-24 01:36:51 +0600

    RED: cast error handling not working

--- backend/middleware/error.js
+++ backend/middleware/error.js
@@ -1,4 +1,13 @@
+const ErrorHandler = require("../utils/errorHandler")
+
 module.exports = (err, req, res, next) => {
+
+    // wrong mongodb id error handling
+    if (err.name === "CastError") {
+        const message = `Resource not found ${err.path}`
+        err = new ErrorHandler(message, 400)
+    }
+
     res.status(err.status).json({
         success: false,
         message: err.message,
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,14 @@ const app = require('./app.js')
 const dotenv = require('dotenv')
 const connectDatabase = require('./config/database.js')
 
+// handling uncaught exception
+process.on("uncaughtException", (err) => {
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server due to uncaughtException`);
+
+    process.exit(1)
+})
+
 //config
 dotenv.config({ path: 'backend/config/config.env' })
 
@@ -12,7 +20,7 @@ const server = app.listen(process.env.PORT, () => {
     console.log(`Listening on ${process.env.PORT}`);
 })
 
-// unhandled promise rejection
+// unhandled promise rejection // AT THE BOTTOM
 process.on("unhandledRejection", (err) => {
     console.log(`Error: ${err.message}`);
     console.log(`Shutting down the server due to unhandled promise rejection`);
