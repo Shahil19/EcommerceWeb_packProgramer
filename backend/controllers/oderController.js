8a9daf5a925ae6de37e58f5304ae664955b9389a
SHAHIL <basharshahil@gmail.com>
2022-10-08 01:34:33 +0600

    BE: get single orders details API created  with populate method using to bring user details also

--- backend/controllers/oderController.js
+++ backend/controllers/oderController.js
@@ -1,5 +1,33 @@
 const Order = require("../models/orderSchema")
+const ErrorHandler = require("../utils/errorHandler")
 
+// ---------------- GET controllers
+// get order details
+exports.getSingleOrder = async (req, res, next) => {
+
+    try {
+        const order = await Order.findById(req.params.id).populate("user", "name email")
+        // here populate method brings the user details 
+        // from the ref - reference was given on orderSchema
+        // user argument tells which field to populate
+        // name email arguments tells which values to bring in
+
+        if (!order) return next(new ErrorHandler("order not found with this id", 404))
+
+        res.status(200).json({
+            success: true,
+            order
+        })
+    } catch (error) {
+        return res.status(404).json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+
+// ---------------- POST controllers
 // create new order
 exports.createOrder = async (req, res, next) => {
     const { shippingInfo, orderItems, paymentInfo, itemsPrice, taxPrice, shippingPrice, totalPrice } = req.body
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,8 +1,12 @@
 const express = require("express")
-const { createOrder } = require("../controllers/oderController")
+const { createOrder, getSingleOrder } = require("../controllers/oderController")
 const { isAuthorizedUser } = require("../middleware/isAuthorized")
 const router = express.Router()
 
+// get order details
+router.route("/order/:id").get(getSingleOrder)
+
+// create new order
 router.route("/order/new").post(isAuthorizedUser, createOrder)
 
 module.exports = router
